f94edc14c8ef912f62fcbae8c4c6379a3ea498c7
Akshay <akshayjohnson117@gmail.com>
2026-02-02 20:24:43 +0530

    tutorials 44

--- route-handlers-demo/app/profile/api/route.ts
+++ route-handlers-demo/app/profile/api/route.ts
@@ -1,3 +1,22 @@
-export async function GET() {
-  return new Response("profile api ");
+import { NextRequest } from "next/server";
+import { headers, cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders=new Headers(request.headers)
+  // console.log(requestHeaders.get("Authorization"));
+
+  const headerList = await headers();
+  console.log(headerList.get("authorization"));
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+  console.log(cookieStore.get("resultsPerPage"));
+
+  return new Response("<h5>profile api </h5>",{
+    headers : {"Content-Type": "text/html",
+                "Set-Cookie":"theme=dark "
+
+    }});
 }
